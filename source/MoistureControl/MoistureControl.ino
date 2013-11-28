11a8ee962fe2c124900106dd6b233f981324c1e1
Ryan Manning <myownsling@gmail.com>
2013-11-27 20:32:44 -0700

    Added sensor polling logic Added water pump state control based on sensor input Added stub for water reservoir shutoff; used to protect the pump   when the water level gets too low (not implemented)

--- source/MoistureControl/MoistureControl.ino
+++ source/MoistureControl/MoistureControl.ino
@@ -1,39 +1,134 @@
 /*
-  ReadAnalogVoltage
-  Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
- 
- This example code is in the public domain.
+  MoistureCotrol
+  Reads the input from a 5V soil moisture sensor and determines whether or not the
+  soil is dry enough to water. Actuates the attached pump based on the input from 
+  the soil moisture sensor.
  */
- 
- int led = 13;
+
+#define DRY 4.25 // Dry threshold at which watering will occur
+#define WET 2.25 // Wet threshold at which watering will stop
+#define OFF 0 // Pump off constant
+#define ON 1 // Pump on constant
+#define POLLING_PERIOD 2500 // Period between sensor polls (ms) when not watering
+#define WATERING_PERIOD 500 // Period between sensor polls (ms) when watering
+
+int led = 13; // LED digital pin
+int pumpRelay = 8; // Water pump relay digital pin
+int soilMoistureAnalogPin = 0; // soil moisture sensor analog pin
+//int state = POLLING; // Set initial state
+int period = POLLING_PERIOD; // Time to wait between sensor polls
+int count = 0; // Number of polls done while watering
+boolean watering = false; // Flag that indicates whether or not the pump is on
+
+// Polls the soil moisture sensor and returns the voltage
+float getSensorVoltage()
+{
+  Serial.println("Soil moisture sensor voltage:");
+  float moistureVoltage = analogRead(soilMoistureAnalogPin) * (5.0 / 1023.0);
+  Serial.print("\t");
+  Serial.println(moistureVoltage); // Print to serial for debug purposes
+  return moistureVoltage;
+}
+
+// Checks to see if the water reservoir used for pumping is empty or not
+boolean reservoirEmpty()
+{
+  // Not implemented yet
+  // This will eventually check another sensor (level sensor or similar) to
+  //   see if there is water in the reservoir
+  return false;
+}
+
+// Turns the pump relay on or off depending on the input
+void actuatePump(int state)
+{
+  if (state == OFF)
+  {
+    // Turn the pump relay off
+    
+    // This test prevents duplicate messages
+    if (watering)
+    {
+      Serial.println("Done watering.");
+    }
+    digitalWrite(pumpRelay, LOW); // Turn the LED off since we are no longer pumping
+    digitalWrite(led, LOW);
+    watering = false;
+  }
+  else if (state == ON)
+  {
+    // Turn the pump relay on
+    if(!reservoirEmpty())
+    {
+      Serial.println("Watering...");
+      digitalWrite(pumpRelay, HIGH); // Turn the LED on to indicate that we are watering
+      digitalWrite(led, HIGH);
+      watering = true;
+    }
+    else
+    {
+      Serial.println("Cannot water, reservoir empty");
+    }
+  }
+}
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  pinMode(led, OUTPUT);
-}
+  Serial.begin(9600); // initialize serial communication at 9600 bits per second:
+  pinMode(led, OUTPUT); // set the LED pin
+  pinMode(pumpRelay, OUTPUT); // Set the pin used to actuate the water pump relay
+}  
 
 // the loop routine runs over and over again forever:
-void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
-  Serial.println(voltage);
+void loop()
+{
+  // Spit out some text formatting for easy reading
+  Serial.println("");
+  Serial.println("********************");
   
-  digitalWrite(led, HIGH);
-  delay(1000);
-  digitalWrite(led, LOW);
-  delay(1000);
-  digitalWrite(led, HIGH);
-  delay(100);
-  digitalWrite(led, LOW);
-  delay(100);
-  digitalWrite(led, HIGH);
-  delay(100);
-  digitalWrite(led, LOW);
-  delay(100);
+  // Poll the moisture sensor and spit out the data on watering time (current or previous)
+  float moisture = getSensorVoltage();
+  Serial.print("Watering/watered for ");
+  Serial.print((float)((float)count * (float)WATERING_PERIOD / 1000.0));
+  Serial.print(" seconds");
+  Serial.println("");
+  
+  // Run a check to see if the reservoir is empty
+  if(reservoirEmpty())
+  {
+    Serial.println("WARNING: reservoir is empty");
+  }
+  
+  // If the sensor voltage goes above the DRY treshold, then start watering
+  if (moisture >= DRY)
+  {
+    if(reservoirEmpty())
+    {
+      Serial.println("Water reservoir is empty, pump will not activate");
+    }
+    else
+    {
+      period = WATERING_PERIOD;
+      if (!watering)
+      {
+        count = 0;
+      }
+      actuatePump(ON);
+    }
+  }
+  // If the sensor voltage goes below the WET threshold, then stop watering
+  else if (moisture <= WET)
+  {
+    period = POLLING_PERIOD;
+    actuatePump(OFF);
+  }
+  
+  if (watering)
+  {
+    // Currently watering, so we will increment the counter
+    count++;
+  }
+  
+  // Don't do anything for a bit
+  delay(period);
 }
